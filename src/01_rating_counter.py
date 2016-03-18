08667b70efc58f9cbb09a38b2cff2b6464702d33
alexcomu <alex.comunian@gmail.com>
2016-03-18 15:54:10 +0100

    added simple example on MapReduce

---
+++ src/01_rating_counter.py
@@ -0,0 +1,16 @@
+from mrjob.job import MRJob
+
+# Rating Counter
+
+class MRRatingCounter(MRJob):
+
+    def mapper(self, key, value):
+        (userID, movieID, rating, timestamp) = value.split('\t')
+        yield rating, 1
+
+    def reducer(self, rating, occurences):
+        yield rating, sum(occurences)
+
+
+if __name__ == '__main__':
+    MRRatingCounter.run()
